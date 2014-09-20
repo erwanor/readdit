a9edd33d99159063c866a54ca1bd2afc49302a26
Ad <adnorth@users.noreply.github.com>
2014-09-20 09:32:59 -0400

    skeleton of the application

---
+++ src/app.js
@@ -0,0 +1,67 @@
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+var req = new XMLHttpRequest();
+  req.open('GET', 'http://www.reddit.com/r/worldnews/top/.json', true);
+  req.onload = function(e) {
+    if (req.readyState == 4 && req.status == 200) {
+      if(req.status == 200) {
+        var title = JSON.parse(req.title);
+        Pebble.sendAppMessage({ "title": title });
+      } else { console.log("Error sending data from reddit"); }
+    }
+  };
+
+var main = new UI.Card({
+  title: '',
+  subtitle: 'READIT',
+  body: 'Press any button.'
+});
+
+main.show();
+
+main.on('click', 'up', function(e) {
+  var menu = new UI.Menu({
+    sections: [{
+      items: [{
+        title: 'Pebble.js',
+        icon: 'images/menu_icon.png',
+        subtitle: 'Can do Menus'
+      }, {
+        title: 'Second Item',
+        subtitle: 'Subtitle Text'
+      }]
+    }]
+  });
+  menu.on('select', function(e) {
+    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+    console.log('The item is titled "' + e.item.title + '"');
+  });
+  menu.show();
+});
+
+main.on('click', 'select', function(e) {
+  var wind = new UI.Window();
+  var textfield = new UI.Text({
+    position: new Vector2(0, 50),
+    size: new Vector2(144, 30),
+    font: 'gothic-24-bold',
+    text: 'Text Anywhere!',
+    textAlign: 'center'
+  });
+  wind.add(textfield);
+  wind.show();
+});
+
+main.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('6969 upvotes');
+  card.subtitle('nyt.com');
+  card.body('The Islamic State is upset with the French government\'s new name for them.');
+  card.show();
+});
