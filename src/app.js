3d186acbe5bb84136ce3809fe3320bc709f0fbd3
Gautam Gupta <GautamGupta@users.noreply.github.com>
2014-09-21 10:32:46 -0400

    Accelerometer.

--- src/app.js
+++ src/app.js
@@ -8,11 +8,13 @@ var Accel = require('ui/accel');
 var debug = 1;
 var counter = 0; // parseInt(localStorage.getItem('counter')) || 0; // If the variable counter exists, pull it from the "localStorage" else initialize to zero.
 var URL = "http://reddit.com/r/worldnews/top/.json";
-var items;
+var items = [];
+var start_time = new Date().valueOf(), new_time, time_diff;
+var start_force = 12, new_force, force_diff;
+var pass = 0;
 
 // Vibration for installation
-if (debug == 1)
-  Vibe.vibrate('short');
+if (debug == 1) Vibe.vibrate('short');
 
 // Initial UI
 var main = new UI.Card({
@@ -30,6 +32,12 @@ Ajax({ url: URL, type: 'json' }, function(resp) {
 
 // Accelerometer
 Accel.init();
+/*
+Accel.config({
+  rate: 25,
+  samples: 1,
+  subscribe: true
+});*/
 
 // Scroll the list up or down
 // This function is called on button click and accelerometer thing
@@ -37,7 +45,6 @@ function scroll_list(way) {
   main.title(items.children[counter].data.score + " upvotes");
   main.subtitle(items.children[counter].data.domain);
   main.body(items.children[counter].data.title);
-  main.show();
   
   if (debug == 1) console.log("Counter: " + counter + "\n");
   
@@ -61,19 +68,81 @@ main.on('click', function(e) {
       scroll_list(e.button);
       break;
       
-    // Preserved for link (page and comments) opening
+    // Link (page and comments) opening
     case 'select' :
       Pebble.openURL("http://reddit.com/" + items.children[counter].data.permalink + ".compact");
-      //Pebble.openURL("http://google.com");
       break;
+      
     default :
       if (debug == 1) console.log('lolwut');
       break;
   }
 });
-/*
+
 main.on('accelTap', function(e) {
-  // if (debug == 1) console.log(util2.format('Tapped accel axis $axis $direction!', e));
-  scroll_list(e.direction > 0 ? 'down' : 'up');
+  /* if (pass > 0) {
+    pass--;
+    return;
+  } */
+  scroll_list(e.direction > 0 ? 'up' : 'down');
+  // pass += 1;
+});
+
+/*
+function sleep(millis) {
+    setTimeout(function()
+            { }
+    , millis);
+}
+*/
+
+/*
+Accel.on('data', function(e) {
+  if (pass > 0) {
+    pass--;
+    return;
+  }
+
+  if (e.accel.vibe != 0) return;
+
+  new_time = new Date().valueOf();
+  time_diff = new_time - start_time;
+  start_time = new_time;
+
+  new_force = e.accel.y * 0.05;
+  force_diff = start_force - new_force;
+  start_force = new_force;
+
+  if (force_diff < -25) {
+    pass += 1;
+    scroll_list('down');
+  } else if (force_diff > 25) {
+    pass += 1;
+    scroll_list('up');
+    // sleep(500);
+  }
+
+  /*
+  new_time = new Date().valueOf();
+  time_diff = new_time - start_time;
+  
+  if (e.accel.vibe != 0 || time_diff < 500 ) return;
+
+  new_force = e.accel.y * 0.05;
+  force_diff = start_force - new_force;
+  start_force = new_force;
+
+  if (force_diff < -25 && time_diff ) {
+    start_time = new_time;
+    console.log('Down' + ' at ' + time_diff);
+    sleep(2000);
+  } else if (force_diff > 25) {
+    start_time = new_time;
+    console.log('Up' + ' at ' + time_diff);
+    sleep(1500);
+  }
+  *
+
+  // console.log('Force diff: ' + force_diff + ' at ' + time_diff);
 });
 */
